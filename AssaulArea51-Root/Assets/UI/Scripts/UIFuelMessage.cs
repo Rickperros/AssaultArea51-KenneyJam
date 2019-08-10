b3dd0b2cca02af402536f21ebe8d31f50db7c84a
GorkaMerindano <gorka.merindano@gmail.com>
2019-08-10 17:42:18 +0200

    Alien Alert

---
+++ AssaulArea51-Root/Assets/UI/Scripts/UIFuelMessage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFuelMessage : MonoBehaviour
+{
+    public string Message;
+
+    public Text TxtMessage;
+    public SpriteRenderer AnchorSprite;
+
+    private Vector3 AnchorPosition;
+
+    private void Update()
+    {
+
+        if (AnchorSprite == null)
+            return;
+
+        AnchorPosition = AnchorSprite.transform.position + Vector3.down * AnchorSprite.bounds.max.y;
+        transform.position = Camera.main.WorldToScreenPoint(AnchorPosition);
+
+    }
+
+    public void FuelLow ()
+    {
+        Message = "Fuel Low";
+        TxtMessage.text = Message;
+    }
+
+    public void OutOfFuel()
+    {
+        Message = "Out of FUel";
+        TxtMessage.text = Message;
+    }
+}
