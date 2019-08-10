fae5c787ea25b415b02f114a1696095bbbc6a475
GorkaMerindano <gorka.merindano@gmail.com>
2019-08-11 01:18:33 +0200

    Alien dying animation

--- AssaulArea51-Root/Assets/Code/Scripts/Alien.cs
+++ AssaulArea51-Root/Assets/Code/Scripts/Alien.cs
@@ -10,37 +10,25 @@ public class Alien : MonoBehaviour
     public float MoveSpeed;
     public float AbducingSpeed;
     public float TimeOnCar;
+    public float TimeExploding;
 
-    enum STATES { TOCAR, ONCAR, ABDUCING, LEAVING, DYING };
+    enum STATES { TOCAR, ONCAR, ABDUCING, LEAVING, EXPLODING, DYING };
     private STATES currentState;
 
     private float onCarTimer = 0;
+    private float onExplodingTimer = 0;
     private Vector3 spawnPosition;
-
+    private Animation animator;
 
     private void Start()
     {
+        animator = GetComponent<Animation>();
         spawnPosition = transform.position;
         ChangeState(STATES.TOCAR);
     }
 
     private void ChangeState (STATES newState)
     {
-        //Exit
-        switch (currentState)
-        {
-            case STATES.TOCAR:
-                break;
-            case STATES.ONCAR:
-                break;
-            case STATES.LEAVING:
-                break;
-            case STATES.DYING:
-                break;
-            case STATES.ABDUCING:
-                break;
-        }
-
         switch (newState)
         {
             case STATES.TOCAR:
@@ -58,6 +46,10 @@ public class Alien : MonoBehaviour
                 break;
             case STATES.ABDUCING:
                 break;
+            case STATES.EXPLODING:
+                animator.Play("Alien Dying");
+                onExplodingTimer = 0;
+                break;
         }
 
         currentState = newState;
@@ -71,13 +63,13 @@ public class Alien : MonoBehaviour
         switch (currentState)
         {
             case STATES.TOCAR:
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3 (Car.transform.position.x, Car.transform.position.y + 8, 0) , MoveSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(Car.transform.position.x, Car.transform.position.y + 8, 0), MoveSpeed * Time.deltaTime);
                 break;
             case STATES.ONCAR:
                 onCarTimer += Time.deltaTime;
                 break;
             case STATES.LEAVING:
-                transform.position = Vector3.MoveTowards(transform.position,spawnPosition, MoveSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, spawnPosition, MoveSpeed * Time.deltaTime);
                 break;
             case STATES.DYING:
                 transform.position += Vector3.up * 10 * Time.deltaTime;
@@ -85,18 +77,21 @@ public class Alien : MonoBehaviour
             case STATES.ABDUCING:
                 Car.transform.position = Vector3.MoveTowards(Car.transform.position, transform.position, AbducingSpeed * Time.deltaTime);
                 break;
+            case STATES.EXPLODING:
+                onExplodingTimer += Time.deltaTime;
+                break;
         }
     }
 
     private void ExitConditions ()
     {
-        if (HP < 0)
-            ChangeState(STATES.DYING);
+        if (HP < 0 && currentState != STATES.EXPLODING && currentState != STATES.DYING)
+            ChangeState(STATES.EXPLODING);
 
         switch (currentState)
         {
             case STATES.TOCAR:
-                if (Mathf.Abs (transform.position.x - Car.transform.position.x) < 1)
+                if (Mathf.Abs(transform.position.x - Car.transform.position.x) < 1)
                     ChangeState(STATES.ONCAR);
                 break;
             case STATES.ONCAR:
@@ -108,8 +103,12 @@ public class Alien : MonoBehaviour
             case STATES.DYING:
                 break;
             case STATES.ABDUCING:
-                if (Mathf.Abs (transform.position.y - Car.transform.position.y) < 4)
+                if (Mathf.Abs(transform.position.y - Car.transform.position.y) < 4)
                     ChangeState(STATES.LEAVING);
+                break;
+            case STATES.EXPLODING:
+                if (onExplodingTimer > TimeExploding)
+                    ChangeState(STATES.DYING);
                 break;
         }
     }
