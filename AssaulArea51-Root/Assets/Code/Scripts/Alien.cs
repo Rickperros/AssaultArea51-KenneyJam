e0dd16b9cee4b148884a8087d6e25eae75ca48e5
GorkaMerindano <gorka.merindano@gmail.com>
2019-08-10 21:25:46 +0200

    faeul partioles

---
+++ AssaulArea51-Root/Assets/Code/Scripts/Alien.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public int HP = 100;
+
+    private void Update()
+    {
+        if (HP < 0)
+            gameObject.SetActive(false);
+    }
+}
--- AssaulArea51-Root/Assets/Code/Scripts/Components/CanisterWeapon.cs
+++ AssaulArea51-Root/Assets/Code/Scripts/Components/CanisterWeapon.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class CanisterWeapon : MonoBehaviour
 {
-    
+    public Transform Sight;
+
+    private ParticleSystem ps;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        { ps.enableEmission = true; }
+        else if (Input.GetMouseButtonUp(0))
+        { ps.enableEmission = false; }
+
+        transform.LookAt(Sight);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+
+        if (other.GetComponent<Alien>())
+            other.GetComponent<Alien>().HP--;
+    }
+
 }
