f494948d5b3f718415f529eea7419a1974c26538
GorkaMerindano <gorka.merindano@gmail.com>
2019-08-10 16:06:42 +0200

    Canister + Car Fuel + Car Low Fuel + Level Progress

---
+++ AssaulArea51-Root/Assets/UI/Scripts/UICanister.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICanister : MonoBehaviour
+{
+
+    [SerializeField] private Animation ArrowsUp;
+    [SerializeField] private Animation ArrowsDown;
+
+    [SerializeField] private Image FuelProgress;
+
+    [SerializeField] private Color CanisterFull;
+    [SerializeField] private Color CanisterEmpty;
+
+    /*
+    float test;
+    private void Update()
+    {
+        test += Time.deltaTime / 4;
+        if (test > 1)
+            test = 0;
+
+        Progress(test);
+    }
+    */
+
+    public void Refill ()
+    {
+        ArrowsUp.gameObject.SetActive(true);
+        ArrowsDown.gameObject.SetActive(false);
+    }
+
+    public void Fill ()
+    {
+        ArrowsUp.gameObject.SetActive(false);
+        ArrowsDown.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Changes the fillable bar of the action
+    /// </summary>
+    /// <param name="value">value from 0 to 1</param>
+    public void Progress(float value)
+    {
+        FuelProgress.color = Color.Lerp(CanisterEmpty, CanisterFull, value);
+        FuelProgress.fillAmount = value / 1;
+    }
+
+}
---
+++ AssaulArea51-Root/Assets/UI/Scripts/UIProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIProgressBar : MonoBehaviour
+{
+    [SerializeField] private Image ImgProgress;
+    [SerializeField] private Transform TCars;
+
+    [SerializeField] private Transform Start;
+    [SerializeField] private Transform Goal;
+
+    /*
+    float test;
+    private void Update()
+    {
+        test += Time.deltaTime / 4;
+        if (test > 1)
+            test = 0;
+
+        Progress(test);
+    }
+    */
+
+    /// <summary>
+    /// Changes the fillable bar of the action
+    /// </summary>
+    /// <param name="value">value from 0 to 1</param>
+    public void Progress (float value)
+    {
+        TCars.transform.position = new Vector3(Mathf.Lerp(Start.transform.position.x + 50, Goal.transform.position.x - 50, value), TCars.transform.position.y);
+        ImgProgress.fillAmount = value / 1;
+    }
+}
